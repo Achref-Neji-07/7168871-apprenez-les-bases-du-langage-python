9ff7c531c4bccd79982bc5944d3e35d7a59cb502
Achref-Neji-07 <107352518+Achref-Neji-07@users.noreply.github.com>
2024-09-25 19:31:39 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+#Écrivez votre code ici !
+fruits=["pomme","banane","orange"]
+print(fruits)
+fruits.append("Kiwi")
+print(fruits)
+fruits.remove("orange")
+print(fruits)
+fruits[2]=("ananas")
+print(fruits)
+print(len(fruits))
+fruits.sort()
+print(fruits) 
